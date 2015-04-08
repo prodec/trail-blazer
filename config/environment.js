9ea8fe7320d697c3b03a8c6bcc22ca0f0364ae55
wallacyyy <leonardowrc@gmail.com>
2015-04-08 16:26:05 -0300

    Add leaflet with google maps tile.

--- config/environment.js
+++ config/environment.js
@@ -28,14 +28,10 @@ module.exports = function(environment) {
   }
 
   if (environment === 'test') {
-    // Testem prefers this...
     ENV.baseURL = '/';
     ENV.locationType = 'none';
-
-    // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
-
     ENV.APP.rootElement = '#ember-testing';
   }
 
