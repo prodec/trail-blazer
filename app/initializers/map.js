60b063a7bc7438158ca67a08688823e65df8a555
Leonardo Couto <leonardowrc@gmail.com>
2015-04-09 13:23:02 -0300

    Initialize map through ember.

--- app/initializers/map.js
+++ app/initializers/map.js
@@ -1,1 +1,21 @@
-//import Ember from 'ember';
+import Ember from 'ember';
+
+var Leaflet = Ember.Object.extend({
+  map: null,
+
+  initMap: function() {
+    var map = new L.Map('map', { center: new L.LatLng(51.51, -0.11), zoom: 13 });
+    var googleLayer = new L.Google('SATELLITE');
+    map.addLayer(googleLayer);
+    this.set('map', map);
+  }.on('init')
+});
+
+export default {
+  name: 'map',
+
+  initialize: function (container, application) {
+    container.register('leaflet:main', Leaflet);
+    application.inject('view', 'leaflet', 'leaflet:main');
+  }
+};
