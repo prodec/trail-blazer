4a8e1c01192942eff6595ba4966213e415ec6730
Leonardo Couto <leonardowrc@gmail.com>
2015-04-10 17:52:14 -0300

    Add navigation menu.

--- Brocfile.js
+++ Brocfile.js
@@ -7,11 +7,20 @@ var app = new EmberApp();
 
 app.import('vendor/leaflet/dist/leaflet.js');
 app.import('vendor/leaflet/dist/leaflet.css');
+app.import('vendor/Buttons/css/buttons.min.css');
+app.import('vendor/Buttons/js/buttons.js');
+app.import('vendor/font-awesome/css/font-awesome.css');
 
 var images = pickFiles('vendor/leaflet/dist/images', {
   srcDir: '/',
   files: ['*'],
   destDir: '/assets/images'
+});
+
+var fonts = pickFiles('vendor/font-awesome/fonts', {
+    srcDir: '/',
+    files: ['*'],
+    destDir: '/fonts'
 });
 
 var static = pickFiles('static', {
@@ -20,4 +29,4 @@ var static = pickFiles('static', {
   destDir: '/assets'
 });
 
-module.exports = mergeTrees([app.toTree(), images, static]);
+module.exports = mergeTrees([app.toTree(), images, fonts, static]);
