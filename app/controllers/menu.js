d5cfe75d42fdd00b4216455821b2d543dfaf2280
Leonardo Couto <leonardowrc@gmail.com>
2015-04-16 18:35:16 -0300

    Refactor map loading and some htmls.

---
+++ app/controllers/menu.js
@@ -0,0 +1,7 @@
+export default Ember.Controller.extend({
+  sessions: ["slideout-goto",
+             "slideout-marker",
+             "slideout-line",
+             "slideout-battery",
+             "slideout-tie"] 
+});
