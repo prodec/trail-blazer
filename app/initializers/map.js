616311080f53a4934534476dcdb7e3f52f5770b2
Leonardo Couto <leonardowrc@gmail.com>
2015-04-16 18:35:16 -0300

    Refatora loading de mapa e html.

--- app/initializers/map.js
+++ app/initializers/map.js
@@ -1,21 +1,37 @@
 import Ember from 'ember';
 
-var Leaflet = Ember.Object.extend({
+var LeafletInit = Ember.Object.extend({
   map: null,
+  initMap: function (options) {
+    var container = Ember.$('<div/>');
+    var map;
 
-  initMap: function() {
-    var map = new L.Map('map', { center: new L.LatLng(51.51, -0.11), zoom: 13 });
+    options = options || {};
+
+    container.attr('id', options.containerId || 'map');
+
+    var map = new L.Map(container.get(0), { center: new L.LatLng(51.51, -0.11), zoom: 13 });
     var googleLayer = new L.Google('SATELLITE');
     map.addLayer(googleLayer);
+
     this.set('map', map);
-  }.on('init')
+    this.set('container', container);
+  }.on('init'),
+
+  attachTo: function ($el) {
+    var container = this.get('container'),
+    map = this.get('map');
+
+    container.appendTo($el);
+    map.invalidateSize(true);
+  }
 });
 
 export default {
   name: 'map',
 
   initialize: function (container, application) {
-    container.register('leaflet:main', Leaflet);
+    container.register('leaflet:main', LeafletInit);
     application.inject('view', 'leaflet', 'leaflet:main');
   }
 };
