e456d85f373626160f5dedbad1ebe1ee10715c22
Leonardo Couto <leonardowrc@gmail.com>
2015-04-17 13:26:50 -0300

    Extract button templates.

--- app/controllers/menu.js
+++ app/controllers/menu.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  sessions: ["slideout-goto",
-             "slideout-marker",
-             "slideout-line",
-             "slideout-battery",
-             "slideout-tie"] 
+  sessions: [ "slideout-goto",
+              "slideout-marker",
+              "slideout-line",
+              "slideout-battery",
+              "slideout-tie" ],
+  buttons: [
+    { id: "goto", class: "button-royal", icon: "fa-location-arrow" },
+    { id: "marker", class: "button-action", icon: "fa-map-marker" },
+    { id: "line", class: "button-caution", icon: "fa-share-alt" },
+    { id: "battery", class: "button-highlight", icon: "fa-flag" },
+    { id: "tie", class: "button-primary", icon: "fa-anchor" }
+  ]
 });
