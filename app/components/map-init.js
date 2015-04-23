7b7702ca41a85230094e1efa4d6823e6b8f69d90
Leonardo Couto <leonardowrc@gmail.com>
2015-04-23 01:40:24 -0300

    Add new project structure with react, flux, browsersync and gulp.

--- app/components/map-init.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  classNames: ['content'],
-
-  initLeaflet: function () {
-    this.get('leaflet').attachTo('.content');
-  }.on('didInsertElement')
-});
