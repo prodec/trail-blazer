fa2be490980efb0258e595079ef4af002687dfee
Leonardo Couto <leonardowrc@gmail.com>
2015-04-24 19:02:44 -0300

    Add google tile.

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import App from './components/app';
 import '../css/app.scss';
+import '../../node_modules/leaflet/dist/leaflet.css';
+import App from './components/app';
+import constants from './constants/constants';
 
 React.render(<App />, document.body);
