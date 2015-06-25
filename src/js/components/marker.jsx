94bea123a349e612dd595d8cf17c18405ff3ed61
Leonardo Couto <leonardowrc@gmail.com>
2015-06-25 18:07:04 -0300

    Adiciona seleção de ícones e observação sobre o ponto de interesse.

--- src/js/components/marker.jsx
+++ src/js/components/marker.jsx
@@ -1,19 +1,67 @@
 import React from 'react';
+import classNames from 'classnames';
 
 export default class Marker extends React.Component {
   constructor() {
     super();
-    this.state = { icons: ['birds', 'helicopter', 'high-building', 'mountains', 'power-line', 'radio-station', 'tree'] };
+    this.selectIcon = this.selectIcon.bind(this);
+    this.preSelect = this.preSelect.bind(this);
+
+    let icons = ['radio-station', 'helicopter', 'high-building',
+                 'mountains', 'power-line', 'birds', 'tree'];
+    let sets = this.initClassSets(icons);
+    this.state = {
+      icons: icons,
+      active: 'radio-station',
+      sets: sets
+    };
+    this.preSelect();
+  }
+
+  initClassSets(icons) {
+    let reducer = (sets, item) => {
+      sets[item] = { 'icon-map': true, 'icon-map-selected': false };
+      return sets;
+    }
+
+    return icons.reduce(reducer, {});
+  }
+  
+  preSelect() {
+    this.state.sets['radio-station']['icon-map-selected'] = true;
+  }
+
+  selectIcon(e) {
+    let current = e.currentTarget.id;
+    this.setState((state, props) => {
+      let isActive = (current == state.active);
+
+      if (!isActive) { state.sets[state.active]['icon-map-selected'] = false }
+      state.sets[current]['icon-map-selected'] = true;
+      state.active = current;
+
+      return state;
+    });
   }
 
   render() {
     return(
       <div id="icon-selection">
-        {this.state.icons.map((icon) => {
+        {this.state.icons.map((icon, i) => {
           return(
-            <img src={"/src/images/" + icon + ".png"} className="icon-map-selection" />
+            <a href="javascript:void(0)" key={i}>
+              <img src={"/src/images/" + icon + ".png"} 
+                   className={classNames(this.state.sets[icon])}
+                   key={i}
+                   onClick={this.selectIcon}
+                   id={icon} />
+            </a>
           ) 
         })}
+
+        <div id="icon-info" className="pure-form">
+          <textarea id="icon-info-description" className="pure-input-1-2" placeholder="Observações"></textarea>
+        </div>
       </div>
     )
   }
