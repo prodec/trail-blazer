c67f0c5703f04fb2f5bbacdb69dfe935cd308d13
Leonardo Couto <leonardowrc@gmail.com>
2015-07-17 14:15:53 -0300

    Change leaflet behavior.

--- src/js/components/popup.jsx
+++ src/js/components/popup.jsx
@@ -3,28 +3,9 @@ import { MarkerConstants } from '../constants/constants';
 import Actions from '../actions/actions';
 import classNames from 'classnames';
 import mapStore from '../stores/mapStore';
+import Popup from '../utils/popup';
 
-export default class Popup {
-  constructor(marker, text, id) {
-    this.marker = marker;
-    this.text = text;
-    this.options = { offset: MarkerConstants.POPUP_OFFSET,
-                     className: 'marker-popup',
-                     minWidth: 200,
-                     autoClose: false };
-    this.id = id;
-  }
-
-  bindOnMarker() {
-    let body = `<div id =${this.id}></div>`;
-    this.marker.bindPopup(body, this.options).openPopup();
-
-    React.render(<PopupComponent text={this.text} />,
-                  document.getElementById(this.id));
-  }
-}
-
-class PopupComponent extends React.Component {
+export default class PopupUI extends React.Component {
   constructor() {
     super();
     this.state = { contentSet: { 'popup-content': true } }
@@ -41,7 +22,6 @@ class PopupComponent extends React.Component {
           <b>{this.props.text}</b>
         </div>
         <div className='popup-actions'>
-          <a href='#'>Mover</a>
           <a href='#' className='popup-action-link'>Editar</a>
           <a href='#'>Remover</a>
         </div>
