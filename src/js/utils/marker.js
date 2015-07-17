b6fccd7a709ecc9f557c715fe22f35adc1e67880
Leonardo Couto <leonardowrc@gmail.com>
2015-07-17 14:15:53 -0300

    Change leaflet behavior.

---
+++ src/js/utils/marker.js
@@ -0,0 +1,45 @@
+import Actions from '../actions/actions';
+import mapStore from '../stores/mapStore';
+import { ModeConstants } from '../constants/constants';
+
+export default class Marker {
+  constructor(latLng, icon) {
+    this.icon = icon;
+    let options = {
+      icon,
+      draggable: true
+    }
+    let marker = new L.Marker(latLng, options);
+    return this.withHooks(marker);
+  }
+
+  withHooks(marker) {
+    let iconMargin = 0; 
+    let icon = this.icon;
+
+    marker.on('click', (e) => {
+      e.target.getPopup()._isOpen = false;
+      $(e.target.getPopup()._container).css('display', 'block');
+    })
+
+    marker.on('add', () => {
+      $('.leaflet-popup').css('display', 'none')
+    });
+
+    marker.on('dragstart', (e) => {
+      let icon = e.target._icon; 
+
+      if (!iconMargin) {
+        iconMargin = parseInt(L.DomUtil.getStyle(icon, 'marginTop'));
+      }
+    
+      icon.style.marginTop = (iconMargin - 15)  + 'px';
+    });
+    
+    marker.on('dragend', (e) => {
+      e.target._icon.style.marginTop = iconMargin + 'px';
+    });
+
+    return marker;
+  }
+}
---
+++ src/js/utils/popup.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { MarkerConstants } from '../constants/constants';
+import PopupUI from '../components/popup'
+
+export default class Popup {
+  constructor(marker, text, id) {
+    this.marker = marker;
+    this.text = text;
+    this.options = { offset: MarkerConstants.POPUP_OFFSET,
+                     className: 'marker-popup',
+                     minWidth: 200,
+                     autoClose: false };
+    this.id = id;
+  }
+
+  bindOnMarker() {
+    let body = `<div id =${this.id}></div>`;
+    this.marker.bindPopup(body, this.options).openPopup();
+
+    React.render(<PopupUI text={this.text} />,
+                  document.getElementById(this.id));
+  }
+}
+
