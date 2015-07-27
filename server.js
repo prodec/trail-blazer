2a8ed66b3eb1d45dba82cb02f38311b9d4b16d05
wallacyyy <leonardowrc@gmail.com>
2015-07-27 15:58:57 -0300

    Habilita cores do webpack-dev-server.

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var config = require('./webpack.config');
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
+  historyApiFallback: true,
+  stats: { colors: true }
 }).listen(3000, 'localhost', function (err) {
   if (err) { console.log(err); }
   console.log('Listening at localhost:3000');
