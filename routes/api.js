53c194accc55c73a9cdf482291284350fd431981
arthurfranca <arthur.a.franca@gmail.com>
2015-07-29 16:59:45 -0300

    Create class to forecast api.

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import WindApi from '../utils/windApi';
 
 let router = express.Router();
 
 router.post('/ping', (_, res) => {
   res.sendStatus(200);
+});
+
+router.get('/windSpeed', async (req, res) => {
+  try {
+    let windSpeed = await new WindApi(req.params.lat,
+                                      req.params.lng,
+                                      req.params.time).getWindSpeed();
+    res.send({ windSpeed });
+  } catch (error) { res.status(500).send({ error }); }
 });
 
 export default router;
