c3d7eacaf9a9e00da4a91fca38871fe092f3c2c9
Leonardo Couto <leonardowrc@gmail.com>
2015-08-07 16:31:27 -0300

    Add test to modeStore.

---
+++ src/js/stores/__tests__/modeStore-test.js
@@ -0,0 +1,27 @@
+jest
+  .dontMock('../store')
+  .dontMock('../modeStore')
+  .dontMock('keymirror');
+
+describe('ModeStore', () => {
+  let ActionConstants = require('../../constants/constants').ActionConstants;
+  let modeStore, dispatcher, callback;
+
+  const mode = 'mode';
+
+  let actionChangeMode = {
+    type: ActionConstants.CHANGE_MODE,
+    mode: 'mode' 
+  };
+
+  beforeEach(() => {
+    modeStore = require('../modeStore');
+    dispatcher = modeStore.dispatcher;
+    callback = dispatcher.register.mock.calls[0][0];
+  });
+
+  it('sets a mode as active', () => {
+    callback(actionChangeMode);
+    expect(modeStore.getState().active).toEqual(mode);
+  });
+});
