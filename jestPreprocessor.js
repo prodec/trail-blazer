0fb20cb7ec07f84a4ab30829d5bf8d202bbcc6c8
arthurfranca <arthur.a.franca@gmail.com>
2015-08-12 16:41:43 -0300

    Add css and tests for wind speed widget.

--- jestPreprocessor.js
+++ jestPreprocessor.js
@@ -4,7 +4,8 @@ module.exports = {
   process: function (src, filename) {
     var options = {
       filename: filename,
-      stage: 0
+      stage: 0,
+      optional: ['runtime']
     };
 
     if (filename.indexOf('node_modules') === -1 && babel.canCompile(filename)) {
