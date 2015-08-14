f1bd4ac10b160bf41931144c68c2b62c9b11472e
Leonardo Couto <leonardowrc@gmail.com>
2015-08-14 13:08:48 -0300

    Fix code style.

--- src/js/utils/__tests__/popup-test.js
+++ src/js/utils/__tests__/popup-test.js
@@ -8,16 +8,16 @@ describe('Popup', () => {
   let Popup = require('../popup');
 
   it('binds the popup to the marker and open it', () => {
-    let marker = new Object();
-    let leafletPopup = new Object();
+    let marker = {};
+    let leafletPopup = {};
 
     leafletPopup.openPopup = jest.genMockFunction();
     marker.bindPopup = jest.genMockFunction().mockReturnValue(leafletPopup);
     React.render = jest.genMockFunction();
 
-    let popup = new Popup(marker, 'text', 1);  
+    let popup = new Popup(marker, 'text', 1);
     popup.bindOnMarker();
     expect(marker.bindPopup).toBeCalledWith(popup.body,
-                                            popup.options)
+                                            popup.options);
   });
 });
