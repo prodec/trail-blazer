e59bddb5502df7e872df744ecdc09ccfecc35dc8
Yuri Pourre <yuriamayuri@gmail.com>
2015-08-14 11:02:13 -0400

    ClientSocket refactoring

--- src/js/stores/altitudeStore.js
+++ src/js/stores/altitudeStore.js
@@ -1,43 +1,13 @@
 import L from 'leaflet';
 import Store from './store';
-import ClientSocket from '../utils/clientSocket';
+import AltitudeSocket from '../utils/socket/altitudeSocket';
 
 class AltitudeStore extends Store {
   constructor() {
     super();
     this.data = { ws: null, altitude: 0};
-    this.data.ws = new ClientSocket(this.receiveMessage);
-    this.keepAlive();
-  }
-
-  keepAlive() {
-    //15 seconds
-    let interval = 15000;
-
-    setInterval(() => {
-      let time = Date.now();
-      let message = JSON.stringify({
-        command: "ping",
-        sent_at: time
-      });
-      
-      this.data.ws.send(message);
-    }, interval);
-  }
-
-  requestHeight(latLng) {
-    let time = Date.now();
-
-    let message = JSON.stringify({
-      command: "point_altitude",
-      sent_at: time,
-      payload: {
-        lat: latLng.lat,
-        lng: latLng.lng
-      }
-    });
-
-    this.data.ws.send(message);
+    this.data.ws = new AltitudeSocket(this.receiveMessage);
+    this.data.ws.keepAlive();
   }
 
   receiveMessage = (message) => {
@@ -53,7 +23,7 @@ class AltitudeStore extends Store {
     return this.dispatcher.register((action) => {
       switch(action.type) {
         case this.ActionConstants.MOUSE_MOVE_ON_MAP:
-          this.requestHeight(action.latLng);
+          this.data.ws.requestHeight(action.latLng);
           this.emitChange();
           break;
 
