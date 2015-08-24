8cd8bcacb9a0a9a65a41707269ff0812ea6c4fcf
arthurfranca <arthur.a.franca@gmail.com>
2015-08-24 12:20:00 -0300

    Dropped cdns in favor of modules.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 // Styles
+import 'leaflet/dist/leaflet.css';
 import 'purecss';
+import 'material-design-lite/material.min.css';
 import '../css/lib/buttons.css';
 import '../css/lib/glyphicons.css';
 import '../css/lib/style.css';
 import '../css/app.scss';
 
 // JS
+import 'material-design-lite/material.min.js';
 import './lib/buttons';
 
 // React
